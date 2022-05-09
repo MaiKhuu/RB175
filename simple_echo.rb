a8c5e7d4d7e2103d57c19ab40a974cb7e0c163ba
Mai Khuu <khuu.tuyetmai@gmail.com>
2022-05-09 22:09:03 +0000

    simple echo server

---
+++ simple_echo.rb
@@ -0,0 +1,13 @@
+require "socket"
+
+# server = TCPServer.new("localhost", 3003)
+server= TCPServer.new(ENV["IP"], ENV["PORT"])
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  puts request_line
+
+  client.puts request_line
+  client.close
+end
