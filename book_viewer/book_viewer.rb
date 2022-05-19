8360e7154c7e8f13859c1e1b3af539df8f8a1672
Mai Khuu <khuu.tuyetmai@gmail.com>
2022-05-19 17:58:39 +0000

    book_viewer project update

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,6 +1,19 @@
+require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @main_title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt") # this returns an array
+
+  erb :home
+end
+
+get "/chapters/1" do
+  @main_title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+  @chapter_content= File.read("data/chp1.txt")
+  @chapter_content.gsub!("\n\n", "<br><br>")
+
+  erb :chapter
 end
