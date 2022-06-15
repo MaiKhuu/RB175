077299d095f29effce00650347f76e5c5a6f3959
Mai Khuu <khuu.tuyetmai@gmail.com>
2022-06-15 18:58:47 +0000

    book viewer, 2nd attempt

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,53 +2,65 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
-before do
-  @contents = File.readlines("data/toc.txt")
-  @main_title = "The Adventures of Sherlock Holmes"
-end
-
 helpers do
   def slugify(text)
-    text.downcase.gsub(/\s+/, "-").gsub(/^[\w-]/, "")
+    text.downcase.gsub(/[\s+]/, "-").gsub(/[^\w-]/,"")
   end
 
-  # returns raw content
-  def content_of_chapter(number)
-    File.read("data/chp#{number}.txt")
-  end
-
-  # returns <p id="chapter1-1"> <p>
-  def in_paragraphs(text, pretext)
+  # put each paragraphs into <p> tags
+  # each <p> tag has an id denoting paragraph number
+  def in_paragraphs(text)
     index = 0
-    text.split("\n\n").map do |paragraph|
+    text.split("\n\n").map do |para|
       index += 1
-      "<p id=\"#{pretext + "-" + index.to_s}\"> #{paragraph}</p>"
+      "<p id=paragraph#{index}>#{para}</p>"
+    end.join
+  end
+
+  # this method iterates through each chapter
+  # takes a block that will receive the chapter's number, title, and content
+  def each_chapter
+    @toc.each_with_index do |chapter_title, index|
+      chapter_number = index + 1
+      chapter_content = File.read("data/chp#{chapter_number}.txt")
+
+      yield chapter_number, chapter_title, chapter_content
     end
   end
 
-  # return ARRAY containing CHAPTER NUMBERS with matching query
-  def chapters_matching(query)
-    result = []
-    (0..@contents.size-1).to_a.each do |index|
-      result << (index + 1) if content_of_chapter(index+1).include?(query)
-    end
-    result
+  def highlight(query, text)
+    text.gsub(query, "<strong>#{query}</strong>")
   end
 
-  # returning ARRAY containing PARAGRAPH NUMBERS with matching query
-  def paragraphs_matching(query, chapter_paragraphs)
+  # return an ARRAY of HASHES {chapter number, chapter title, matching paragraphs}
+  # matching query case SENSITIVE
+  def chapter_matching(query)
+    return nil if query.nil? || query.empty?
+
     result = []
-    chapter_paragraphs.each_with_index do |para, index|
-      result << index  if para.include?(query)
+    matches = []
+    each_chapter do |chapter_number, chapter_title, chapter_content|
+      chapter_content.split("\n\n").each_with_index do |para, index|
+        matches << {id: "paragraph#{index+1}", content: highlight(query, para)} if para.include?(query)
+      end
+
+      result << {number: chapter_number, title: chapter_title, paragraphs: matches} unless matches.empty?
+      matches = []
     end
+
     result
   end
 
 end
 
-# not_found do
-#   redirect "/"
-# end
+before do
+  @title = "The Adventures of Sherlock Holmes"
+  @toc = File.readlines("data/toc.txt")
+end
+
+not_found do
+  redirect "/"
+end
 
 get "/" do
   erb :home
@@ -56,14 +68,12 @@ end
 
 get "/chapters/:number" do
   @chapter_number = params[:number].to_i
-  redirect "/" unless (1..@contents.size).to_a.include?(@chapter_number)
-
-  @chapter_title = @contents[@chapter_number - 1]
-  @chapter_content_in_paragraph =  in_paragraphs(content_of_chapter(params[:number]), "chapter" + @chapter_number.to_s)
+  @chapter_content = File.read("data/chp#{@chapter_number}.txt")
 
   erb :chapter
 end
 
 get "/search" do
+  @search_result = chapter_matching(params[:query])
   erb :search
 end
